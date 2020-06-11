d48b64c4d900b31e7d7c7fd1d06f874570edd25d
Dong Nguyen <nvdong1992@gmail.com>
2020-06-11 13:15:51 +0700

    Update unit test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,15 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AngularFireMessaging } from '@angular/fire/messaging';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule
       ],
       declarations: [
         AppComponent
+      ],
+      providers: [
+        AngularFireMessaging
       ],
     }).compileComponents();
   }));
@@ -20,16 +29,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Personal Assistant'`, () => {
+  it(`should have as title 'personal-assistant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Personal Assistant');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Personal Assistant app is running!');
+    expect(app.title).toEqual('personal-assistant');
   });
 });
--- frontend/src/app/core/services/messaging.service.spec.ts
+++ frontend/src/app/core/services/messaging.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MessagingService } from './messaging.service';
+import { AngularFireMessaging } from '@angular/fire/messaging';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
 
 describe('MessagingService', () => {
   let service: MessagingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule
+      ],
+      providers: [
+        AngularFireMessaging
+      ],
+    });
     service = TestBed.inject(MessagingService);
   });
 
