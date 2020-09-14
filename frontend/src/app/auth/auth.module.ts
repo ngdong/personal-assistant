843934e9d9f88e99add53d0f2be358a8be2f1634
Dong Nguyen <nvdong1992@gmail.com>
2020-09-14 07:37:40 +0700

    Implement login page

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthenticationService } from '@core/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
 
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      username: this.fb.control(''),
+      password: this.fb.control('')
+    });
+  }
+
+  onSubmit() {
+    this.authService.login(this.loginForm.value);
+  }
 }
